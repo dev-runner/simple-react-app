4ab87e5cb5dd39fcc82b6def280a19ac72792867
przemek <przemek@devrunner.pl>
2018-01-03 12:12:29 +0100

    basic functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import ItemsFilter from './ItemsFilter';
+import ItemsList from './ItemsList';
+import AddItem from './AddItem';
 
+
+// App component
 class App extends React.Component {
     
     constructor(props){
@@ -7,21 +12,43 @@ class App extends React.Component {
 
         this.state = {
             items: [],
-            filter: ''
+            filter: '',
+            newName: '',
+            newEmail: ''
         };
+
+        this.filterItems = this.filterItems.bind(this);
+        this.addItem = this.addItem.bind(this);
     }
 
     componentWillMount(){
+        // fetch initial set of items from remote REST api
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(res => res.json())
             .then(res => this.setState({items: res }));
     }
 
-    filter(e){
+    filterItems(e){
         this.setState({
             filter: e.target.value
         });
-        console.log(this.state.filter);
+    }
+
+    addItem(e){
+        e.preventDefault();
+        
+        let newItem = {
+            id: (this.state.items[this.state.items.length-1].id + 1),
+            name: e.target.newName.value,
+            email: e.target.newEmail.value
+        };
+
+        let items = this.state.items.slice(); // shallow copy of array
+        items.push(newItem);
+
+        this.setState({
+            items: items
+        });
     }
 
     render(){
@@ -34,8 +61,9 @@ class App extends React.Component {
 
         return (
             <div>
-                Filter names: <input type="text" onChange={this.filter.bind(this)} />
-                {items.map(item => <h1 key={item.id}>{item.id}. {item.name}</h1>)}
+                <ItemsFilter label="Filter names" onChange={this.filterItems} />
+                <ItemsList items={items} />
+                <AddItem onSubmit={this.addItem} />
             </div>
         );
     }
